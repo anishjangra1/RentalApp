830ad5c74eaa16e73fddd0b297d1190942b8bbbe
anishjangra1 <jangra.anish@gmail.com>
2021-02-05 23:40:03 +0530

    Ride Plan

---
+++ app/src/main/java/com/ride/GetPlansActivity.kt
@@ -0,0 +1,60 @@
+package com.ride
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.ride.databinding.ActivityGetStartedBinding
+import com.ride.viewmodels.GenerateOtpViewModel
+import com.ride.viewmodels.GetPlanViewModel
+
+class GetPlansActivity : AppCompatActivity(){
+    private var recyclerViewRidePlans: RecyclerView? = null
+    var btnCountinueWithPhone: TextView? = null
+    lateinit var binding: ActivityGetStartedBinding
+    private lateinit var progressBar: ProgressBar
+    private val  viewModel: GetPlanViewModel  by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        setContentView(R.layout.activity_ride_plans)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        recyclerViewRidePlans = findViewById(R.id.recyclerViewRidePlans)
+//        binding.btnCountinueWithPhone.setOnClickListener(clickListener)
+
+//        getPlans()
+    }
+
+    private val clickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.btnCountinueWithPhone -> {
+                val intent = Intent(this, GenerateOtpActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+    private fun getPlans(){
+//                progressBar.visibility = View.VISIBLE
+                viewModel.getPlans()
+            }
+
+        }
+
+
--- app/src/main/java/com/ride/data/AppRepository.kt
+++ app/src/main/java/com/ride/data/AppRepository.kt
@@ -13,5 +13,8 @@ class AppRepository @Inject constructor(private val serviceRest: RestApiService)
     suspend fun generateOtForLogin(mobileNumber: String): Response<String>{
         return serviceRest.generateOtpForLogin(mobileNumber)
     }
+    suspend fun getRidePlans(): Response<String>{
+        return serviceRest.getPlan()
+    }
 
 }
